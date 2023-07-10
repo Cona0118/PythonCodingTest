32341c3cfcc4eba5c803e7e1a8a4a2f428fb0811
Cona <chpark0101@gmail.com>
2023-07-10 06:42:41 +0000

    1일차 / 조건문,반복문 실습사항

--- Day1-2/IF_REPEAT_EX4.py
+++ Day1-2/IF_REPEAT_EX4.py
@@ -6,6 +6,12 @@ for i in range(0,num+1,2):
     Sum += i
 print(Sum)
 
+Sum = 0
+for i in range(num+1):
+    if (i%2 == 0):
+        Sum += i
+print(Sum)
+
 # 문제 2 : 영문 소문자 q가 입력될 까지 입력 문자를 무한 출력하시오.
 # While 문과 if문 활용 
 q = input("소문자 알파벳 입력: ")
