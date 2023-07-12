a70b5bdbd00aa3339b637b584999c2a8857714b2
Cona <chpark0101@gmail.com>
2023-07-12 01:38:43 +0000

    1일차 / 입출력 실습사항

--- Day1-2/DATA_INPUT2.py
+++ Day1-2/DATA_INPUT2.py
@@ -11,3 +11,4 @@ print(b, a)  # 두 변수를 순서 교체 후 한번에 출력
 print("한번에 문자를 2번 입력받습니다 : ", end='')
 c, d = input().split()  # 문자 2개 입력, 공백 기준 분리, 정수형 map(int, input.split()), 실수형 map(float, input.split())
 print(c, d)  # 두 변수를 한번에 출력
+ 
